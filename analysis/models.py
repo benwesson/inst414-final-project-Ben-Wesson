4d2cd1c4c82618aa0d6bf37991f907c99f4487b4
benwesson <benjamin.j.wesson@gmail.com>
2024-08-16 03:13:48 -0400

    docstrings

--- analysis/models.py
+++ analysis/models.py
@@ -11,6 +11,16 @@ from sklearn import linear_model
 
 
 def regressionGraph(col1,col2,df,graphTitle,xAxis,yAxis):
+    """
+    Regression model plotter
+
+    Parameters
+    fileName : CSV file
+        Dataset on crab populations for a given year for a certain state. 
+    
+    Returns
+    df : A pandas dataframe of the CSV file ready for cleaning.
+    """
     sns.regplot(x=col1,y=col2,data = df).set(title=graphTitle)
     saveString = "data/visuals/" + graphTitle + ".png"
     plt.xlabel(xAxis)
@@ -19,15 +29,37 @@ def regressionGraph(col1,col2,df,graphTitle,xAxis,yAxis):
     plt.show()
 
 def predictiveModel(col1,col2,df):
-      varX = df[[col1,col2]]
-      varY = df['Total Number of Crabs in Millions (All Ages)']
-      sk_reg = linear_model.LinearRegression()
-      sk_reg.fit(varX.values,varY.values)
-      predCrabs = sk_reg.predict([[72.7,56.8]])
-      print(predCrabs)
+    """
+    CSV to pandas Dataframe Converter
+
+    Parameters
+    fileName : CSV file
+        Dataset on crab populations for a given year for a certain state. 
+    
+    Returns
+    df : A pandas dataframe of the CSV file ready for cleaning.
+    """
+    varX = df[[col1,col2]]
+    varY = df['Total Number of Crabs in Millions (All Ages)']
+    sk_reg = linear_model.LinearRegression()
+    sk_reg.fit(varX.values,varY.values)
+    predCrabs = sk_reg.predict([[72.7,56.8]])
+    print(predCrabs)
 
 def correlationCoe(df,col1,col2,title):
-      x = df[col1].to_list()
-      y = df[col2].to_list()
-      r = np.corrcoef(x,y)
-      print(title,  ":  ", r)
+      
+
+    """
+    CSV to pandas Dataframe Converter
+
+    Parameters
+    fileName : CSV file
+        Dataset on crab populations for a given year for a certain state. 
+    
+    Returns
+    df : A pandas dataframe of the CSV file ready for cleaning.
+    """
+    x = df[col1].to_list()
+    y = df[col2].to_list()
+    r = np.corrcoef(x,y)
+    print(title,  ":  ", r)
--- etl/transform_and_load.py
+++ etl/transform_and_load.py
@@ -1,23 +1,7 @@
 
 import pandas as pd
-"""
-#Import the raw dataframes from the extract file. No changes have been made yet
-md = extract.marylandDF
-nc = extract.northCarolinaDF
-mdTemp = extract.mdTempDF
 
-print(md.columns)
-#These lists repersent the columns I want to drop in the datasets
-mdList = [2,3,4,5,6]
-ncList = [1,2,3]
-mdTempList = [1,2,3,4,5,6,7,8,9,10,11,12]
 
-#Planned feature
-#Add function to create mergerd CSV with shared years
-#Convert to common units. 
-#Not needed yet because I am at first focusing on measuirng percent change
-
-"""
 
 
 def dropCommas (df):
@@ -52,29 +36,39 @@ def dropColumns (df,list):
 
 #Make sure data is all ints
 def convertData(df1,df2):
+    """
+    Convet column values to numeric
+
+    Parameters
+    df1: pandas dataframe
+        Dataset on crab populations for a given year for Maryland. 
+    df2: pandas dataframe
+         Dataset on seasonal recorded temperatures for a given year for Maryland.
+    
+    Returns
+    df1 : Crab population with all numeric values
+    df2 : Temperture dataset with all numeric values
+    """
     convert1 = {'Year' : int, 'Total Number of Crabs in Millions (All Ages)' : int}
     convert2 = {'Year': int,  'Annual': float,'Winter(Dec-Feb)': float,'Spring(Mar-May)': float,'Summer(Jun-Aug)': float,'Autum(Sep-Nov)': float,}
     df1 = df1.astype(convert1)
     df2 = df2.astype(convert2)
     return df1,df2
 
-#print(mdDrop.dtypes)
+
 
 def mergeDf(df1,df2):
+    """
+    Create merged database with inner join
+
+    Parameters
+    df1 : pandas dataframe
+        Dataset on crab populations for a given year for Maryland. 
+    df2 : pandas dataframe
+    Returns
+    df : A pandas dataframe consisting of the parameter dataframes
+    """
     mergedData = pd.merge(df1,df2,how="inner",on=["Year"])
     return mergedData
-#print(mdTempDrop.dtypes)
-
-"""
-#Merge databases
-
-print(mergedData)
-
-#Write cleaned dataframes to new CSVs
-mergedData.to_csv('data/cleaned/mergedData.csv',header=True, index=False)
-mdDrop.to_csv('data/cleaned/mdDropped.csv',header=True, index=False)
-ncDrop.to_csv('data/cleaned/ncDropped.csv',header=True, index=False)
 
 
-
-"""
